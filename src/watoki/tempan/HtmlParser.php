92010d6a8b38f4fbfe02ee1297df73d59cc716aa
Nikolas Martens <Nikolas.M@rtens.org>
2013-03-09 10:22:55 +0100

    Extracted HTML parser

---
+++ src/watoki/tempan/HtmlParser.php
@@ -0,0 +1,66 @@
+<?php
+namespace watoki\tempan;
+
+class HtmlParser {
+
+    private static $tagPairs = array(
+        '<html><body>' => array(),
+        '<body>' => array('<html>', '</html>'),
+        '<html>' => array('<body>', '</body>'),
+        '' => array('<html><body>', '</body></html>'),
+    );
+
+    /**
+     * @var \DOMDocument
+     */
+    private $doc;
+
+    /**
+     * @var string
+     */
+    private $html;
+
+    function __construct($html) {
+        $this->html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
+    }
+
+    /**
+     * @return \DOMElement
+     */
+    public function getRoot() {
+        if (!$this->doc) {
+            $this->parse();
+        }
+        return $this->doc->documentElement;
+    }
+
+    private function parse() {
+        $this->doc = new \DOMDocument();
+
+        if (!$this->doc->loadHTML($this->html)) {
+            throw new \Exception('Error while parsing mark-up.');
+        }
+    }
+
+    public function toString(\DOMElement $element = null) {
+        if (!$this->doc) {
+            return $this->html;
+        }
+
+        $this->doc->formatOutput = true;
+        $content = $this->doc->saveHTML($element ?: $this->doc->documentElement);
+
+        $content = trim($content);
+
+        $input = trim(preg_replace('/>\s+?</', '><', $this->html));
+        foreach (self::$tagPairs as $match => $replace) {
+            if (substr($input, 0, strlen($match)) == $match) {
+                $content = str_replace($replace, '', $content);
+                break;
+            }
+        }
+
+        return $content;
+    }
+
+}
--- src/watoki/tempan/Renderer.php
+++ src/watoki/tempan/Renderer.php
@@ -3,22 +3,10 @@ namespace watoki\tempan;
 
 class Renderer {
 
-    private static $tagPairs = array(
-        '<html><body>' => array(),
-        '<body>' => array('<html>', '</html>'),
-        '<html>' => array('<body>', '</body>'),
-        '' => array('<html><body>', '</body></html>'),
-    );
-
     /**
-     * @var string
+     * @var HtmlParser
      */
-    private $template;
-
-    /**
-     * @var \DOMElement
-     */
-    private $root;
+    public $parser;
 
     /**
      * @var Element
@@ -36,13 +24,12 @@ class Renderer {
     private $logginEnabled;
 
     function __construct($template) {
-        $this->template = mb_convert_encoding($template, 'HTML-ENTITIES', 'UTF-8');
+        $this->parser = new HtmlParser($template);
     }
 
     public function getRoot() {
         if (!$this->rootElement) {
-            $this->root = $this->createDocument();
-            $this->rootElement = new Element($this->root);
+            $this->rootElement = new Element($this->parser->getRoot());
         }
 
         return $this->rootElement;
@@ -61,34 +48,7 @@ class Renderer {
             $this->log = $animator->getLog();
         }
 
-        return $this->__toString();
-    }
-
-    private function createDocument() {
-        $doc = new \DOMDocument();
-
-        if (!$doc->loadHTML($this->template)) {
-            throw new \Exception('Error while parsing mark-up.');
-        }
-
-        return $doc->documentElement;
-    }
-
-    public function __toString() {
-        $this->getRoot();
-        $doc = $this->root->ownerDocument;
-        $doc->formatOutput = true;
-        $content = $doc->saveHTML($this->root);
-
-        $input = trim(preg_replace('/>\s+?</', '><', $this->template));
-        foreach (self::$tagPairs as $match => $replace) {
-            if (substr($input, 0, strlen($match)) == $match) {
-                $content = str_replace($replace, '', $content);
-                break;
-            }
-        }
-
-        return $content;
+        return $this->parser->toString();
     }
 
     public function enableLogging($enable = true) {
